a439974fd18f5e922f8b32306433f026196ce489
cristian <cchura.cpc@gmail.com>
2025-07-09 22:49:01 -0400

    login servcices

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +10,57 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class Login {
 
+  fb = inject(FormBuilder);
+  authService = inject(Auth)
+  cargando = false;
+
+  // FormGroup
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.required])
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
+
+  // FormBuilder
+  loginForm2 = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+  funIngresar(){
+
+    if(this.loginForm.invalid) return;
+
+    this.cargando = true;
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login({email, password}).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.cargando = false
+      },
+      (error) => {
+        console.log(error)
+        this.cargando = false
+      }
+    );
+    
+   /*
+   this.authService.login({email, password}).subscribe({
+    next: (res) => {
+      this.cargando = false;
+      console.log(res);
+    },
+    error: (err)=> {
+      this.cargando = false;
+      console.log(this.cargando);
+      console.log(err.error);
+    }
+   })
+   */
+  }
+  
+
+
 
 }
