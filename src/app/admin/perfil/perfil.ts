7e14db467719d880c267b2b990d7e6f087b2e606
cristian <cchura.cpc@gmail.com>
2025-07-10 22:24:59 -0400

    login redireccion y localstorage de token

--- src/app/admin/perfil/perfil.ts
+++ src/app/admin/perfil/perfil.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-perfil',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './perfil.scss'
 })
 export class Perfil {
+
+  authService = inject(Auth);
+
+  constructor(){
+    this.authService.perfil().subscribe(
+      (res) => {
+        console.log(res)
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
 
 }
