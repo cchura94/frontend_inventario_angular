8391759f13efe62be959f2810a75d7340faff3cc
cristian <cchura.cpc@gmail.com>
2025-07-10 22:06:01 -0400

    estilos + webLayout en rutas

---
+++ src/app/layout/web-layout/web-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-web-layout',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './web-layout.html',
+  styleUrl: './web-layout.scss'
+})
+export class WebLayout {
+
+}
