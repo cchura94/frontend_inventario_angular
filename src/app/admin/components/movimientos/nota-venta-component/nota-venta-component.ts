d4ae0996151d54d86378360138d97b4cd9ef62fe
cristian <cchura.cpc@gmail.com>
2025-07-28 21:34:50 -0400

    diseño nota de venta

--- src/app/admin/components/movimientos/nota-venta-component/nota-venta-component.ts
+++ src/app/admin/components/movimientos/nota-venta-component/nota-venta-component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProductoService } from '../../../services/producto-service';
+
+interface Column {
+  field: string;
+  header: string;
+  customExportHeader?: string;
+}
 
 @Component({
   selector: 'app-nota-venta-component',
@@ -8,4 +15,55 @@ import { Component } from '@angular/core';
 })
 export class NotaVentaComponent {
 
+  selectedAlmacen = signal('');
+selectedSucursal = signal(-1);
+  sucursales = signal([]);
+  almacenes = signal([])
+  buscar = signal("")
+  products = signal([]);
+  totalRecords = signal(0);
+  loading = signal(false);
+  carrito = signal([
+    {nombre: 'TECLADO', precio: 52.99, cantidad: 2},
+    {nombre: 'MONITOR', precio: 5852.99, cantidad: 1},
+    {nombre: 'ESCRITORIO GAMER', precio: 852.99, cantidad: 1}
+
+  ]);
+
+    cols!: Column[];
+
+  visible = signal(false)
+
+  productoService = inject(ProductoService)
+
+
+  showDialogCliente(){
+    this.visible.set(true);
+  }
+
+  funGetAlmacenes(){
+    
+  }
+  
+  cargarDatos(event: any){
+    let page = event.first / event.rows + 1;
+
+    this.funGetProductos(page, event.rows);
+  }
+
+  addCarrito(prod: any){
+    this.carrito().push({nombre: prod.nombre, precio: prod.precio_venta_actual, cantidad: 1},)
+  }
+
+  funGetProductos(page: number = 1, limit: number = 5){
+    this.loading.set(true)
+    this.productoService.funListar("1", page, limit, this.buscar()).subscribe(
+      (res: any) => {
+        this.products.set(res.data);
+        this.totalRecords.set(res.total);
+
+        this.loading.set(false);
+      }
+    )
+  }
 }
